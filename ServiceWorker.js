233e193835d43e677a0aa3521ef73f1b7ea0e400
José Ribeiro <j.ribeiro@inklusion.pt>
2025-12-07 18:20:29 +0000

    Built with correct webgl template files

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-op-bets-1.02";
+const cacheName = "DefaultCompany-op-bets-1.03";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
@@ -8,10 +8,22 @@ const contentToCache = [
 
 ];
 
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cacheKeepList = [cacheName, "UnityCache_DefaultCompany-op-bets"];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
     e.waitUntil((async function () {
+      await deleteOldCaches();
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
       await cache.addAll(contentToCache);
@@ -20,14 +32,23 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+     const cache = await caches.open(cacheName);
+      let response = await cache.match(e.request);
+      if (response) 
+      { 
+        console.log(`[Service Worker] Fetched resource from cache: ${e.request.url}`);
+        return response; 
+      }
 
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+
+      if(e.request.url.includes("AssetBundles/WebGL"))
+      {
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+      }
+      
       return response;
     })());
 });
